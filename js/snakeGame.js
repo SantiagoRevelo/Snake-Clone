18c7c809c195abed2d9f0035e3dab3d0d3f15c69
sreveloc <sreveloc@gmail.com>
2017-10-26 04:28:49 +0200

    landing screen

--- js/snakeGame.js
+++ js/snakeGame.js
@@ -1,5 +1,3 @@
-var updateRatio = 1000 / 5;
-
 var pixelSize = 20;
 var fieldSize = 25;
 
@@ -13,12 +11,13 @@ var snake;
 var totalLength;
 var currentLength;
 var direction;
+var gameSpeed;
+var updateRatio;
 var food;
 var points;
-
 var gameOver;
-
 var gameLoop;
+var isFirstStart = true;
 
 function Initialize() {
     if (!direction) {
@@ -28,20 +27,25 @@ function Initialize() {
     if (typeof gameLoop != "undefined") {
         clearInterval(gameLoop);
     }
-
-    gameLoop = setInterval(updateGame, updateRatio);
-
     addEventListener('keydown', keyDownHandler, false);
     points = 0;
     gameOver = false;
-    createSnake();
-    createFood();
+
+    if (isFirstStart) {
+        printMainMenu();
+    } else {
+        createSnake();
+        createFood();
+        gameLoop = setInterval(updateGame, updateRatio);
+    }
 }
 
 function createSnake() {
     snake = [];
     totalLength = 5;
     currentLength = 1;
+    gameSpeed = 10;
+    updateRatio = 1000 / gameSpeed;
     // The snake will be created at the center of the canvas
     var startPoint = {
         x: Math.floor(fieldSize * 0.5),
@@ -74,6 +78,7 @@ function createSnake() {
                     x: startPoint.x - i,
                     y: startPoint.y
                 });
+                break;
         }
 
     }
@@ -83,7 +88,7 @@ function createFood() {
     food = {
         x: Math.floor(Math.random() * fieldSize),
         y: Math.floor(Math.random() * fieldSize)
-    }; 
+    };
 }
 
 // Main game loop
@@ -102,17 +107,23 @@ function update() {
 function keyDownHandler(event) {
     if (event) {
         if (!gameOver) {
-            // for each key, check for not back on opposite direction
-            if ((event.key == "w" || event.key == "ArrowUp") && direction != "down") {
-                direction = "up";
-            } else if ((event.key == "d" || event.key == "ArrowRight") && direction != "left") {
-                direction = "right";
-            } else if ((event.key == "s" || event.key == "ArrowDown") && direction != "up") {
-                direction = "down";
-            } else if ((event.key == "a" || event.key == "ArrowLeft") && direction != "right") {
-                direction = "left";
+            if (isFirstStart) {
+                isFirstStart = false;
+                Initialize();
+            } else {
+                // for each key, check for not back on opposite direction
+                if ((event.key == "w" || event.key == "ArrowUp") && direction != "down") {
+                    direction = "up";
+                } else if ((event.key == "d" || event.key == "ArrowRight") && direction != "left") {
+                    direction = "right";
+                } else if ((event.key == "s" || event.key == "ArrowDown") && direction != "up") {
+                    direction = "down";
+                } else if ((event.key == "a" || event.key == "ArrowLeft") && direction != "right") {
+                    direction = "left";
+                }
             }
-        } else {
+        }
+        else {
             Initialize();
         }
     }
@@ -146,24 +157,29 @@ function updateSnake() {
         var tail;
         // Check if there is a piece of food in the next position
         if (nextHeadX == food.x && nextHeadY == food.y) {
-            tail = {x: nextHeadX, y: nextHeadY };
+            tail = {
+                x: nextHeadX,
+                y: nextHeadY
+            };
             points += 10;
+           /* if (points % 10 == 0) {
+                gameSpeed++;
+            }*/
             createFood();
-        }
-        else {
+        } else {
             tail = snake.pop();
             tail.x = nextHeadX;
             tail.y = nextHeadY;
         }
-        
+
         // make the snake grows on each frame untill reaches its length
         if (currentLength < totalLength) {
             snake.push({
                 x: tail.x,
                 y: tail.y
-            });            
+            });
         }
-        
+
         snake.unshift(tail);
         currentLength = snake.length;
     }
@@ -200,7 +216,7 @@ function drawBg() {
 function drawSnake() {
     for (var i = 0; i < snake.length; i++) {
         var c = snake[i];
-        paintCell(c.x, c.y, i == 0 ? "#1f5b1f" : "#526052");        
+        paintCell(c.x, c.y, i == 0 ? "#1f5b1f" : "#526052");
     }
 }
 
@@ -218,7 +234,18 @@ function paintCell(x, y, color) {
 
 // Print the curren points
 function drawPoints() {
-    drawText("Score: " + points, 1, fieldSize -1, "9pt", "#526052", "start");
+    drawText("Score: " + points, 1, fieldSize - 1, "9pt", "#526052", "start");
+}
+
+// Shows the Game Over message to the player
+function printMainMenu() {
+    // fade off the canvas a little
+    ctx.fillStyle = "rgba( 0, 0, 0, 0.7 )";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    drawText("Serpiente", Math.floor(fieldSize * 0.5), Math.floor(fieldSize * 0.5) - 1, "36pt", "#a9b6a9");
+    drawText("- usa los cursores / W,S,A,D -", Math.floor(fieldSize * 0.5), Math.floor(fieldSize * 0.5) + 1, "9pt", "#a9b6a9");
+    drawText("Pulsa una tecla para empezar", Math.floor(fieldSize * 0.5), Math.floor(fieldSize * 0.5) + 3, "9pt", "#a9b6a9");
 }
 
 // Shows the Game Over message to the player
@@ -226,14 +253,14 @@ function printGameOver() {
     // fade off the canvas a little
     ctx.fillStyle = "rgba( 0, 0, 0, 0.7 )";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
+
     drawText("Score:" + points, Math.floor(fieldSize * 0.5), Math.floor(fieldSize * 0.5) - 1, "36pt", "#a9b6a9");
     drawText("- Press any key to restart -", Math.floor(fieldSize * 0.5), Math.floor(fieldSize * 0.5) + 2, "9pt", "#a9b6a9");
 }
 
 // Function for print text on canvas
 function drawText(text, x, y, fontSize, color, align) {
-    if (typeof(align) == "undefined") {
+    if (typeof (align) == "undefined") {
         align = "center";
     }
     ctx.font = fontSize + " 'Press Start 2P'";
